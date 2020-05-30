930abd85e9d4fab9f594b71a78ba8a0727fcb6bd
newgengames <61648800+newgengames@users.noreply.github.com>
2020-05-30 14:41:13 +0530

    Create AllAppend.js

---
+++ content/all/res/append/AllAppend.js
@@ -0,0 +1,35 @@
+$(".top").append(`
+<div class="top-div1">
+    <img src="../res/images/back.png" class="back-img">
+    <div class="cur-page"><span id="all-levels"></span>/<span id="all-total"></span></div>
+    <input type="number" placeholder="Jump to" class="jump-input">
+    <div class="go">Go</div>
+  </div>
+  <div class="top-div2">
+    <div class="top-div2-ti">All Facts</div>
+  </div>
+`);
+
+$(".nav").append(`
+<div class="nav-bg nav-bg1">
+    <img src="../res/images/all1.png" class="nav-img all-img all-img1">
+    <img src="../res/images/all2.png" class="nav-img2 all-img all-img2">
+  </div>
+  <div class="nav-bg nav-bg2">
+    <img src="../res/images/category1.png" class="nav-img all-img category-img1">
+    <img src="../res/images/category2.png" class="nav-img2 all-img category-img2">
+  </div>
+  <div class="nav-bg nav-bg3">
+    <img src="../res/images/star1.png" class="nav-img all-img star-img1">
+    <img src="../res/images/star2.png" class="nav-img2 all-img star-img2">
+  </div>
+  <div class="nav-bg nav-bg4">
+    <img src="../res/images/settings1.png" class="nav-img all-img settings-img1">
+    <img src="../res/images/settings2.png" class="nav-img2 all-img settings-img2">
+  </div>
+`);
+
+$(".hyperlink").append(`
+<div class="previous"></div>
+ <div class="next"></div>
+`);
